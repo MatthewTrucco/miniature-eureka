b4c355076135442e2a84363d45f0bf9b2fb8ad16
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-09-14 14:57:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api_routes);
+app.use('/', html_routes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
